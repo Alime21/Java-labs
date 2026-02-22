01f47a286d5486819d9fef11a9f7f0cb800ce906
UserAlime <alime3888@gmail.com>
2026-02-22 14:09:05 +0300

    Added Week6

---
+++ Java-labs/Week6/app/src/main/java/msku/ceng/madlab/week6/MainActivity.java
@@ -0,0 +1,56 @@
+package msku.ceng.madlab.week6;
+
+import static android.content.res.Configuration.ORIENTATION_PORTRAIT;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.io.Serializable;
+
+public class MainActivity extends AppCompatActivity implements MovieFragment.OnMovieSelected {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+
+    @Override
+    public void movieSelected(Movie movie) {
+        int displayMode = getResources().getConfiguration().orientation;
+        if(displayMode == ORIENTATION_PORTRAIT){
+            Intent intent = new Intent(this, DetailedActivity.class);
+            intent.putExtra("movie", (Serializable) movie);
+            startActivity(intent);
+        }
+        else {
+            DetailedFragment df = (DetailedFragment) getSupportFragmentManager().findFragmentByTag("details");
+            if(df == null){
+                FragmentTransaction fts = getSupportFragmentManager().beginTransaction();
+                df = DetailedFragment.newInstance(movie);
+                fts.add(R.id.container, df, "details");
+                fts.commit();
+            }
+            else {
+                df.setMovie(movie,findViewById(R.id.container));
+            }
+        }
+
+
+
+    }
+}
