f523c9296bef6c4e715c2692b410830cd8cfd065
UserAlime <alime3888@gmail.com>
2026-02-22 14:09:10 +0300

    Added Week11

---
+++ Java-labs/Week11/MyNotesFireBase2/app/src/main/java/msku/ceng/madlab/mynotesfirebase/Note.java
@@ -0,0 +1,35 @@
+package msku.ceng.madlab.mynotesfirebase;
+
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.Exclude;
+
+public class Note {
+    @Exclude  // database e kaydetmeden sadece burada kaydetmeyi sağlar
+    private String id;
+    private Timestamp date;
+    private String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
